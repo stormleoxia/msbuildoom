6b9d4f30d3f3d1e966c3fd33003433c9cf03e241
storm <storm@leoxia.com>
2017-03-01 00:10:41 +0700

    First commit of repro source

---
+++ Xamarin.Standard/LabelView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Standard
+{
+	public partial class LabelView : ContentView
+	{
+		public LabelView ()
+		{
+        }
+
+	    public IReadonlyViewModel Model
+	    {
+            set { BindingContext = value; }
+	    }
+	}
+
+    public interface IReadonlyViewModel
+    {
+        
+    }
+}
